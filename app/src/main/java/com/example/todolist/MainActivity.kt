a02ddede8c851f4c3da9f7c3b9cfaeae4d2497c8
MasaharuDobashi <dobashi.masaharu@gmail.com>
2020-12-05 19:17:49 +0900

    ToDoModelの更新

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var todoModel = ToDoModel().findALll(this)
+        var dataArray = mutableListOf<ToDoModel>()
+
+        if(!todoModel.isEmpty()) {
+            for (todo in todoModel) {
+                Log.d("ToDoModel","todo: ${todo.toDoName}")
+                dataArray.add(todo)
+            }
+//            val adapter = ArrayAdapter<ToDoModel>(
+//                applicationContext,
+//                android.R.layout.simple_list_item_1,
+//                dataArray
+//            )
+//            todoList.adapter = adapter
+        }
+
+        floatingActionButton.setOnClickListener {
+            ToDoModel().addRealm(this)
+        }
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/todolist/ToDoModel.kt
@@ -0,0 +1,52 @@
+package com.example.todolist
+
+import android.content.Context
+import io.realm.Realm
+import io.realm.RealmObject
+import io.realm.RealmResults
+import io.realm.annotations.PrimaryKey
+import java.security.AccessControlContext
+
+
+open class ToDoModel: RealmObject() {
+    @PrimaryKey
+    var id: Long = 0
+//    var createTime:String = ""
+    var toDoName: String = ""
+    var todoDate:String = ""
+    var toDoDetail:String = ""
+
+
+    private fun initRealm(context: Context) : Realm {
+        Realm.init(context)
+        return Realm.getDefaultInstance()
+    }
+
+
+    fun findALll(context: Context) : RealmResults<ToDoModel> {
+        val realm = initRealm(context)
+        return realm.where(ToDoModel::class.java).findAll()
+    }
+
+
+
+
+//    fun addRealm(context: Context, addModel: ToDoModel) {
+    fun addRealm(context: Context) {
+        val realm = initRealm(context)
+        realm.executeTransaction {
+            var id: Long = realm.where(ToDoModel::class.java).count()
+            var quizModel = realm.createObject(ToDoModel::class.java, id)
+            /*
+            quizModel.toDoName = addModel.toDoName
+            quizModel.todoDate = addModel.todoDate
+            quizModel.toDoDetail = addModel.toDoDetail
+            */
+
+            quizModel.toDoName = "TESTtoDoName"
+            quizModel.todoDate = "TESTtodoDate"
+            quizModel.toDoDetail = "TESTtoDoDetail"
+        }
+    }
+
+}
