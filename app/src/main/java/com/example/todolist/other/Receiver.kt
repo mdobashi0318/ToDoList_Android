fbb38067456d9282335e7f3acd96c508670b4f75
MasaharuDobashi <masaharu.dobashi@gmail.com>
2021-12-30 17:25:11 +0900

    ボタンのサイズ、不要なクラス削除

--- app/src/main/java/com/example/todolist/other/Receiver.kt
+++ app/src/main/java/com/example/todolist/other/Receiver.kt
@@ -9,48 +9,12 @@ import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.todolist.R
+import com.example.todolist.screen.MainActivity
 
 class Receiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (context != null) {
-            MyNotification.sendNotification(context)
-        }
-    }
-}
-
-class MyNotification {
-    companion object {
-        private const val NOTIFICATION_CHANNEL_ID = "com.example.todolist"
-        private const val NOTIFICATION_CHANNEL_NAME = "todolist"
-        private const val NOTIFICATION_CHANNEL_DESCRIPTION = "期限がきたら通知を表示します"
-        private const val NOTIFICATION_TITLE = "期限切れのTodoがあります"
-        private const val NOTIFICATION_MESSAGE = ""
-        fun sendNotification(context: Context) {
-            val channelId = NOTIFICATION_CHANNEL_ID
-            val channelName = NOTIFICATION_CHANNEL_NAME
-            val channelDescription = NOTIFICATION_CHANNEL_DESCRIPTION
-
-            //Android 8.0 以上ではアプリの通知チャンネルを登録することが必要。
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val importance = NotificationManager.IMPORTANCE_DEFAULT
-                val channel = NotificationChannel(channelId, channelName, importance).apply {
-                    description = channelDescription
-                }
-                val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                manager.createNotificationChannel(channel)
-
-            }
-
-            //通知をシステムに登録しています。
-            val builder = NotificationCompat.Builder(context, channelId).apply {
-                setSmallIcon(R.drawable.ic_launcher_foreground)
-                setContentTitle(NOTIFICATION_TITLE)
-                setContentText(NOTIFICATION_MESSAGE)
-                priority = NotificationCompat.PRIORITY_DEFAULT
-            }
-
-            val id = 0
-            NotificationManagerCompat.from(context).notify(id, builder.build())
+            MainActivity.sendNotification(context)
         }
     }
 }
--- app/src/main/java/com/example/todolist/screen/MainActivity.kt
+++ app/src/main/java/com/example/todolist/screen/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.todolist.screen
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todolist.*
 import com.example.todolist.model.ToDoModel
@@ -75,6 +81,41 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+        private const val NOTIFICATION_CHANNEL_ID = "com.example.todolist"
+        private const val NOTIFICATION_CHANNEL_NAME = "todolist"
+        private const val NOTIFICATION_CHANNEL_DESCRIPTION = "期限がきたら通知を表示します"
+        private const val NOTIFICATION_TITLE = "期限切れのTodoがあります"
+        private const val NOTIFICATION_MESSAGE = ""
+        fun sendNotification(context: Context) {
+            val channelId = NOTIFICATION_CHANNEL_ID
+            val channelName = NOTIFICATION_CHANNEL_NAME
+            val channelDescription = NOTIFICATION_CHANNEL_DESCRIPTION
+
+            //Android 8.0 以上ではアプリの通知チャンネルを登録することが必要。
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(channelId, channelName, importance).apply {
+                    description = channelDescription
+                }
+                val manager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                manager.createNotificationChannel(channel)
+
+            }
+
+            //通知をシステムに登録しています。
+            val builder = NotificationCompat.Builder(context, channelId).apply {
+                setSmallIcon(R.drawable.ic_launcher_foreground)
+                setContentTitle(NOTIFICATION_TITLE)
+                setContentText(NOTIFICATION_MESSAGE)
+                priority = NotificationCompat.PRIORITY_DEFAULT
+            }
+
+            NotificationManagerCompat.from(context).notify(0, builder.build())
         }
     }
 
