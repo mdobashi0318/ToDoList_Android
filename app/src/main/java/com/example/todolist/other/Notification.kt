78027b3fcb3b88f2483a2211d4e8e6d13b15c56f
MasaharuDobashi <masaharu.dobashi@gmail.com>
2022-10-16 21:53:10 +0900

    Notificationクラスの編集

--- app/src/main/java/com/example/todolist/other/Notification.kt
+++ app/src/main/java/com/example/todolist/other/Notification.kt
@@ -1,15 +1,71 @@
 package com.example.todolist.other
 
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.todolist.R
 import com.example.todolist.screen.TodoApplication
+import com.example.todolist.screen.TodoDetailActivity
 import java.util.*
 
 class Notification {
 
     companion object {
+
+        private const val NOTIFICATION_CHANNEL_ID = "com.example.todolist"
+        private const val NOTIFICATION_CHANNEL_NAME = "todolist"
+        private const val NOTIFICATION_CHANNEL_DESCRIPTION = "期限がきたら通知を表示します"
+        private const val NOTIFICATION_TITLE = "期限切れのTodoがあります"
+
+        fun createChannel(context: Context) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    NOTIFICATION_CHANNEL_NAME,
+                    importance
+                ).apply {
+                    description = NOTIFICATION_CHANNEL_DESCRIPTION
+                }
+                val manager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                manager.createNotificationChannel(channel)
+            }
+        }
+
+        fun sendNotification(context: Context, message: String, createTime: String) {
+            if (createTime.isEmpty()) return
+            val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID).apply {
+                setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                setContentTitle(NOTIFICATION_TITLE)
+                setContentText(message)
+
+                val intent = Intent(context, TodoDetailActivity::class.java).apply {
+                    putExtra("createTime", createTime)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                }
+
+                val pending: PendingIntent = PendingIntent.getActivity(
+                    context,
+                    createTime.toInt(),
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+                setContentIntent(pending)
+                setAutoCancel(true)
+                priority = NotificationCompat.PRIORITY_DEFAULT
+            }
+
+            NotificationManagerCompat.from(context).notify(createTime.toInt(), builder.build())
+        }
+
+
         /**
          * ローカル通知を設定する
          * @param context
--- app/src/main/java/com/example/todolist/other/Receiver.kt
+++ app/src/main/java/com/example/todolist/other/Receiver.kt
@@ -1,17 +1,8 @@
 package com.example.todolist.other
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.example.todolist.R
-import com.example.todolist.screen.MainActivity
-import com.example.todolist.screen.TodoDetailActivity
 
 class Receiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent) {
@@ -19,7 +10,7 @@ class Receiver : BroadcastReceiver() {
             val message = intent.getStringExtra("title") ?: ""
             val createTime = intent.getStringExtra("createTime") ?: ""
 
-            MainActivity.sendNotification(
+            Notification.sendNotification(
                 context,
                 message,
                 createTime
