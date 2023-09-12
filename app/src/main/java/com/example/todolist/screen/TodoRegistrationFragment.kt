554755c10f150eed04137c7d676773883391739c
MasaharuDobashi <masaharu.dobashi@gmail.com>
2023-09-13 00:35:27 +0900

    TodoRegistrationFragmentの編集

--- app/src/main/java/com/example/todolist/screen/TodoRegistrationFragment.kt
+++ app/src/main/java/com/example/todolist/screen/TodoRegistrationFragment.kt
@@ -192,26 +192,19 @@ class TodoRegistrationFragment : Fragment() {
     private suspend fun setObserver() {
         val args: TodoRegistrationFragmentArgs by navArgs()
         viewModel.setModel(args.createTime)
-        val titleObserver = Observer { title: String ->
-            binding.titleEditText.setText(title)
-        }
 
-        val detailObserver = Observer { detail: String ->
-            binding.detailEditText.setText(detail)
+        viewModel.toDoName.observe(viewLifecycleOwner) {
+            binding.titleEditText.setText(it)
         }
-
-        val dateObserver = Observer { date: String ->
-            binding.dateTextView.text = date
+        viewModel.toDoDetail.observe(viewLifecycleOwner) {
+            binding.detailEditText.setText(it)
         }
-
-        val timeObserver = Observer { time: String ->
-            binding.timeTextView.text = time
+        viewModel.todoDate.observe(viewLifecycleOwner) {
+            binding.dateTextView.text = it
         }
-
-        viewModel.toDoName.observe(viewLifecycleOwner, titleObserver)
-        viewModel.toDoDetail.observe(viewLifecycleOwner, detailObserver)
-        viewModel.todoDate.observe(viewLifecycleOwner, dateObserver)
-        viewModel.todoTime.observe(viewLifecycleOwner, timeObserver)
+        viewModel.todoTime.observe(viewLifecycleOwner) {
+            binding.timeTextView.text = it
+        }
     }
 
     /**
